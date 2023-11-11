02247294db9415b33f992da01112b3cca0dff158
Dhimas Ferdiansyah <starblackdest@gmail.com>
2023-11-11 14:53:01 +0700

    update

--- components/Form.tsx
+++ components/Form.tsx
@@ -23,12 +23,14 @@ const Form = () => {
             placeholder="Title"
             className="bg-gray-100 border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
             required
+            maxLength={100}
           />
           <textarea
             name="content"
             placeholder="Content"
             className="bg-gray-100 border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
             required
+            maxLength={500}
           />
         </div>
         <div className="mx-auto">
